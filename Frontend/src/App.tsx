a6e1511ec88369d1a538b8fe04dd2256757083e0
Stev3eM <stefan-mergl@gmx.de>
2023-10-25 12:48:50 +0200

    First Frontend Code for Landing Page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/Logo_no_background.svg';
+import pic1 from './assets/Landingpage_noodles_in_bowl.svg';
+import pic2 from './assets/Landingpage_noodles_with_basi.svg';
+import pic3 from './assets/Landingpage_meat_pieces.svg';
+import './Frontend.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <div className= "body">
+            <h2>
+                Tired of standing in front of the stove feeling bored, cooking the same recipes? Welcome to our exciting cooking app, where culinary adventures await! Explore a world of flavor explosions and creative recipes that will elevate your cooking skills. Ready for the adventure in your kitchen? Let's cook together!
+            </h2>
+
+            <img src={pic1} className="Landingpage_noodles_in_bowl" alt="bowl" />
+            <img src={pic2} className="Landingpage_noodles_with_basi" alt="basel" />
+            <img src={pic3} className="Landingpage_meat_pieces" alt="meat" />
+
+            <a
+                className="Login-link"
+                href="https://hurensohn.solutions/de-de/index.html"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Login
+            </a>
+        </div>
+      </body>
     </div>
+
   );
 }
 
