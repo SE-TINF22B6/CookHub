bad8b3ce4130c4e9c4227855a3865d001bbffea5
Stev3eM <stefan-mergl@gmx.de>
2023-11-20 11:55:14 +0100

    Created AboutUsPage.tsx

--- Frontend/src/AboutUsPage.tsx
+++ Frontend/src/AboutUsPage.tsx
@@ -1,5 +1,22 @@
+import logo from "./assets/Logo_no_background.svg";
+import React from "react";
+
 export default function AboutUsPage(){
     return(
-        <h1>Das sind wir</h1>
+        <div className="AboutUsPage">
+            <header className="header">
+                <title>About us</title>
+            </header>
+            <div className="body">
+                <h1>About us</h1>
+                <br/>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <br/>
+                <li><a href='login'>Login</a></li> <li>
+                <a href='faqs'>FAQ's</a></li>
+                <li><a href='about'>About us</a></li>
+            </div>
+
+        </div>
     );
 }
