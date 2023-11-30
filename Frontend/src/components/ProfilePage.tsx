d21d8e084fb1aaae8ddaec41d9e8d83127d01bd9
Stev3eM <stefan-mergl@gmx.de>
2023-11-30 16:46:00 +0100

    -> start of ProfilePage.tsx -> start of ProfilePage.css

--- Frontend/src/components/ProfilePage.tsx
+++ Frontend/src/components/ProfilePage.tsx
@@ -1,1 +1,47 @@
-export {}
+import React from "react";
+import "../design/ProfilePage.css";
+import logo from "../assets/Logo_no_background.svg"
+import topBackground from "../assets/ProfilePage/EllipseBackgroundTop.png"
+import logoutBTN from "../assets/ProfilePage/Logout.png";
+import settingsBTN from "../assets/ProfilePage/Settings.png";
+
+export default function ProfilePage() {
+    return (
+        <div className="ProfilePage">
+            <header className="App-header">
+
+                <div className="topBackground">
+                    <img src={topBackground}/>
+                </div>
+
+                <img src={logo} className="App-logo" alt="logo"/>
+
+
+
+                <div className="Buttons">
+                    <img src={logoutBTN} className="LogoutButton" alt="logout button"/>
+                    <img src={settingsBTN} className="SettingsButton" alt="settings button"/>
+                </div>
+
+
+
+
+
+
+            </header>
+
+            <div className="body">
+
+
+
+
+
+
+
+
+
+            </div>
+
+        </div>
+    );
+}
