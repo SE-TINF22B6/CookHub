9893a76170844d17f45577a04cdc853631835711
Stev3eM <stefan-mergl@gmx.de>
2023-11-30 20:29:55 +0100

    -> update of profile page tsx & css

--- Frontend/src/components/ProfilePage.tsx
+++ Frontend/src/components/ProfilePage.tsx
@@ -5,7 +5,6 @@ import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
 import "../design/ProfilePage.css";
 import logo from "../assets/Logo_no_background.svg"
-import background from "../assets/ProfilePage/Background.png";
 import avatarUsernameHolder from "../assets/ProfilePage/AvatarUsernameHolder.png"
 import logoutBTN from "../assets/ProfilePage/Logout.png";
 import settingsBTN from "../assets/ProfilePage/Settings.png";
@@ -13,40 +12,45 @@ import frameCards from "../assets/ProfilePage/FrameCardsCenter.png";
 
 export default function ProfilePage() {
     return (
-
         <div className="ProfilePage">
-            <Container fluid="md">
-                <Row>
-                    <Col>
-                        <Stack direction="horizontal" gap={3}>
-                            <div className="p-1">
-                                <a href="/"><img src={logo} className="App-logo" alt="logo"/></a>
-                            </div>
-                            <div className="p-2">
-                                <img src={avatarUsernameHolder} className="AvatarUsernameHolder" alt="Avatar&Username Background"/>
-                            </div>
-                            <div className="p-3">
-                                <Stack gap={2}>
-                                    <div className="p-2">
-                                        <a href="/logout"><img src={logoutBTN} className="LogoutButton" alt="logout button"/></a>
-                                    </div>
-                                    <div className="p-2">
-                                        <a href="/settings"><img src={settingsBTN} className="SettingsButton" alt="settings button"/></a>
-                                    </div>
-                                </Stack>
-                            </div>
-                        </Stack>
-                    </Col>
-                </Row>
-            </Container>
 
-            <Container fluid="md">
-                <Row>
-                    <Col>
-                        <img src={frameCards} alt="frameCards"/>
-                    </Col>
-                </Row>
-            </Container>
+                <Container fluid="md" className="ContainerTop" style={{backgroundColor:"transparent"}}>
+                    <Row>
+                        <Col>
+                            <Stack className="Stack1" direction="horizontal" gap={4}>
+                                <div className="p-1">
+                                    <a href="/"><img src={logo} className="App-logo" alt="logo"/></a>
+                                </div>
+                                <div className="p-2">
+                                    <img src={avatarUsernameHolder} className="AvatarUsernameHolder" alt="Avatar&Username Background"/>
+                                </div>
+                                <div className="p-3">
+                                    <Stack className="Stack2" gap={2}>
+                                        <div className="p-2">
+                                            <a href="/logout"><img src={logoutBTN} className="LogoutButton" alt="logout button"/></a>
+                                        </div>
+                                        <div className="p-2">
+                                            <a href="/settings"><img src={settingsBTN} className="SettingsButton" alt="settings button"/></a>
+                                        </div>
+                                    </Stack>
+                                </div>
+                            </Stack>
+                        </Col>
+                    </Row>
+                </Container>
+
+                <Container fluid="md" className="ContainerMid">
+                    <Row>
+                        <Col>
+                            <img src={frameCards} alt="frameCards"/>
+                        </Col>
+                    </Row>
+                </Container>
+
+
+
+
+
 
         </div>
     );
