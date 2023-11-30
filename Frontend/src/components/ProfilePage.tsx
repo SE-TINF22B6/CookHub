57dc10c8b9db46db29e8a2e4f75c113f626d7e78
Stev3eM <stefan-mergl@gmx.de>
2023-11-30 19:38:49 +0100

    -> update of profile page tsx & css

--- Frontend/src/components/ProfilePage.tsx
+++ Frontend/src/components/ProfilePage.tsx
@@ -1,38 +1,52 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
 import "../design/ProfilePage.css";
 import logo from "../assets/Logo_no_background.svg"
-import topBackground from "../assets/ProfilePage/EllipseBackgroundTop.png"
-
+import background from "../assets/ProfilePage/Background.png";
+import avatarUsernameHolder from "../assets/ProfilePage/AvatarUsernameHolder.png"
 import logoutBTN from "../assets/ProfilePage/Logout.png";
 import settingsBTN from "../assets/ProfilePage/Settings.png";
+import frameCards from "../assets/ProfilePage/FrameCardsCenter.png";
 
 export default function ProfilePage() {
     return (
+
         <div className="ProfilePage">
-            <header className="App-header">
+            <Container fluid="md">
+                <Row>
+                    <Col>
+                        <Stack direction="horizontal" gap={3}>
+                            <div className="p-1">
+                                <a href="/"><img src={logo} className="App-logo" alt="logo"/></a>
+                            </div>
+                            <div className="p-2">
+                                <img src={avatarUsernameHolder} className="AvatarUsernameHolder" alt="Avatar&Username Background"/>
+                            </div>
+                            <div className="p-3">
+                                <Stack gap={2}>
+                                    <div className="p-2">
+                                        <a href="/logout"><img src={logoutBTN} className="LogoutButton" alt="logout button"/></a>
+                                    </div>
+                                    <div className="p-2">
+                                        <a href="/settings"><img src={settingsBTN} className="SettingsButton" alt="settings button"/></a>
+                                    </div>
+                                </Stack>
+                            </div>
+                        </Stack>
+                    </Col>
+                </Row>
+            </Container>
 
-                <img src={topBackground} alt="topBackground"/>
-
-                <div>
-                    <img src={logo} className="App-logo" alt="logo"/>
-                </div>
-                <div>
-
-                </div>
-
-
-                <div className="Buttons">
-                    <img src={logoutBTN} className="LogoutButton" alt="logout button"/>
-                    <img src={settingsBTN} className="SettingsButton" alt="settings button"/>
-                </div>
-            </header>
-
-
-            <div className="body">
-
-
-
-            </div>
+            <Container fluid="md">
+                <Row>
+                    <Col>
+                        <img src={frameCards} alt="frameCards"/>
+                    </Col>
+                </Row>
+            </Container>
 
         </div>
     );
