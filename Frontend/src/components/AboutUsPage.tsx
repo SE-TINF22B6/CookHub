555e047660052d1af1bb8484a52b68da1d07eb73
Stev3eM <stefan-mergl@gmx.de>
2023-12-04 21:43:18 +0100

    // Bugfixing // -> fixed white area of all pages to be background -> optimized several size relations of pages to look more page-fitted

--- Frontend/src/components/AboutUsPage.tsx
+++ Frontend/src/components/AboutUsPage.tsx
@@ -1,16 +1,26 @@
 import team from "../assets/CookHub_Team_&_Roles.png";
 import '../design/AboutUsPage.css';
 import React from "react";
+import logo from "../assets/Logo_no_background.svg";
+import 
 
 export default function AboutUsPage(){
     return(
         <div className="AboutUsPage">
-            <header className="header">
-                <title>About us</title>
+
+
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <ul>
+                    <a className='FAQs-link' target="_self" href='/faqs'>FAQ's</a>
+                    <a className='About-link' target="_self" href='/about'>About us</a>
+                    <a className='Impressum-link' target="_self" href='/impressum'>Impressum</a>
+                </ul>
             </header>
+
+
             <div className="body">
-                <h1>About us</h1>
-                <br/>
+
                 <img src={team} className="Team_Roles" alt="team" width="1000" height="500"/>
                 <br/>
                 <ul>
--- Frontend/src/components/LoginPage.tsx
+++ Frontend/src/components/LoginPage.tsx
@@ -4,6 +4,8 @@ import React, {useState} from "react";
 import "../design/LoginPage.css"
 import chef from "../assets/Chef_Carlo_without_background (1).png";
 import {UserClient} from "../clients/UserClient";
+import Item from '@mui/material/Grid'
+import Grid from '@mui/material/Grid'
 
 export default function LoginPage() {
 
@@ -13,6 +15,8 @@ export default function LoginPage() {
 
     return (
         <div className="LoginPage">
+
+
             <header className="App-header">
                 <a href="/">
                     <img src={logo} className="App-logo" alt="logo"/>
@@ -25,70 +29,64 @@ export default function LoginPage() {
                 </ul>
             </header>
 
-            <div className="container LoginPageBody text-center">
 
-                <div className="row">
+            <div className="containerLoginPageBodyAndText-Center">
+                <Grid container spacing={0}>
+                    <Grid item xs={5}>
+                        <Item>
+                            <div className="EnterInformation">
 
-                    <div className="col-6">
-                        {/*<h1 className="StartAdventure">Start your Eatventure!</h1><br/><br/>*/}
+                                <form className={"container col-md-6"}>
+                                    <div className="InputMail">
+                                        <label form="exampleInputEmail1" className="form-label">Email address</label>
+                                        <input type="email" className="form-control" id="exampleInputEmail1"
+                                               aria-describedby="emailHelp"
+                                               onInput={event => email = event.currentTarget.value}></input>
+                                    </div>
+                                    <div className="InputPassword">
+                                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                                        <input type="password" className="form-control" id="exampleInputPassword1"
+                                               onInput={event => password = event.currentTarget.value}></input>
+                                    </div>
+                                    <div style={{fontSize: 12}}>{message}</div>
 
-                        <div className="EnterInformation">
-
-                            <form className={"container col-md-6"}>
-                                <div className="InputMail">
-                                    <label form="exampleInputEmail1" className="form-label">Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1"
-                                           aria-describedby="emailHelp"
-                                           onInput={event => email = event.currentTarget.value}></input>
-                                </div>
-                                <div className="InputPassword">
-                                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1"
-                                            onInput={event => password = event.currentTarget.value}></input>
-                                </div>
-                                <div style={{fontSize: 12}}>{message}</div>
-
-                                <input type="button" className="SubmitLogin" value="Submit"
-                                   onClick={() => new UserClient().sendLoginRequest(email, password).then(response => setMessage(JSON.parse(response)))}/>
-                            </form>
-
-                            <br/>
-
-                            <h3>
-                                <span style={{fontSize: 12}}>Don't have an account yet?</span>
+                                    <input type="button" className="SubmitLogin" value="Submit"
+                                           onClick={() => new UserClient().sendLoginRequest(email, password).then(response => setMessage(JSON.parse(response)))}/>
+                                </form>
                                 <br/>
-                                <a className="SubmitButton" href='/signup'>Sign up</a>
-                            </h3>
-                        </div>
-                    </div>
+                                <h3>
+                                    <span style={{fontSize: 18}}>Don't have an account yet?</span>
+                                    <br/>
+                                    <a className="SubmitButton" style={{fontSize: 20}} href='/signup'>Sign up</a>
+                                </h3>
+                            </div>
+                        </Item>
+                    </Grid>
 
-
-                    <div className="col-6">
-
-                        <div className='speech-bubble'>
-                            <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Ready</span> to dive into a world of culinary delights? <br/>
-                            <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Log in</span> to get access to a treasure trove of recipes,
-                            cooking tips, and foodie inspiration. <br/>
-                            <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Join</span> our community of cooking enthusiasts
-                            and elevate your kitchen experience. Whether it's your first time here or you're
-                            returning for more culinary adventures, we're thrilled to have you aboard. <br/> <br/>
-                            <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Let's get coooooking!
+                    <Grid item xs={3}>
+                        <Item>
+                            <div className='speech-bubble'>
+                                <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Ready</span> to dive into a world of culinary delights? <br/>
+                                <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Log in</span> to get access to a treasure trove of recipes,
+                                cooking tips, and foodie inspiration. <br/>
+                                <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Join</span> our community of cooking enthusiasts
+                                and elevate your kitchen experience. Whether it's your first time here or you're
+                                returning for more culinary adventures, we're thrilled to have you aboard. <br/> <br/>
+                                <span style={{color:'#C9FE71', backgroundColor:'lightgrey', fontWeight:'bold'}}>Let's get coooooking!
                             </span>
-                        </div>
+                            </div>
 
-                        <h2 className="LoginPageText"> </h2>
+                            <h2 className="LoginPageText"> </h2>
 
-                        <div className="Loginpage_images">
-                            <img src={chef} className="Chef-Carlo2" alt="chef" />
-                        </div>
-
-                    </div>
-
-
-                </div>
-
-
+                            <div className="Loginpage_images">
+                                <img src={chef} className="Chef-Carlo2" alt="chef" />
+                            </div>
+                        </Item>
+                    </Grid>
+                </Grid>
             </div>
+
+
         </div>
     );
 }
