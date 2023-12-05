e3a79f36d51d83f5b1257bde9fe6eddf509bc407
Stev3eM <stefan-mergl@gmx.de>
2023-12-05 06:41:34 +0100

    // Global Navbar // -> created a Navbar/Header which  can be placed on every page
    // Bugs //
    -> after opened menu popup the  <ClickAwayListener> only respond when clicking near the popup => it should be respond everywhere in the browser area

--- Frontend/src/components/ButtonAppBar.tsx
+++ Frontend/src/components/ButtonAppBar.tsx
@@ -10,7 +10,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Logo from "../assets/Logo_no_background.svg";
 import Avatar from "../assets/Hotdog.svg";
-import menuList from "./MenuListComposition";
 import MenuListComposition from "./MenuListComposition";
 
 
@@ -34,7 +33,7 @@ export default function ButtonAppBar() {
                 <Toolbar className="ToolBar">
 
 
-                    <img className="Logo" src={Logo} style={{width: '5%'}} />
+                    <img className="Logo" src={Logo} alt='Logo' style={{width: '5%'}} />
 
                     <Typography
                         className="LinksPages"
@@ -48,11 +47,11 @@ export default function ButtonAppBar() {
                             aria-label="outlined button group"
                         >
                             <Button className='MyRecipes' target="_self" href='/myrecipes' sx={{ color: '#c7fc70' }}>My Recipes</Button>
-                            <Button className='RecipesBrowser' target="_self" href='/recipesbrowser'sx={{ color: '#c7fc70' }}>Recipes Browser</Button>
-                            <Button className='RecipeCreator' target="_self" href='/recipecreator'sx={{ color: '#c7fc70' }}>Recipe Creator</Button>
-                            <Button className='AboutUs' target="_self" href='/aboutus'sx={{ color: '#c7fc70' }}>About Us</Button>
-                            <Button className='FAQs' target="_self" href='/faqs'sx={{ color: '#c7fc70' }}>FAQs</Button>
-                            <Button className='Impressum' target="_self" href='/impressum'sx={{ color: '#c7fc70' }}>Impressum</Button>
+                            <Button className='RecipesBrowser' target="_self" href='/recipesbrowser' sx={{ color: '#c7fc70' }}>Recipes Browser</Button>
+                            <Button className='RecipeCreator' target="_self" href='/recipecreator' sx={{ color: '#c7fc70' }}>Recipe Creator</Button>
+                            <Button className='AboutUs' target="_self" href='/aboutus' sx={{ color: '#c7fc70' }}>About Us</Button>
+                            <Button className='FAQs' target="_self" href='/faqs' sx={{ color: '#c7fc70' }}>FAQs</Button>
+                            <Button className='Impressum' target="_self" href='/impressum' sx={{ color: '#c7fc70' }}>Impressum</Button>
                         </ButtonGroup>
                     </Typography>
 
@@ -75,7 +74,7 @@ export default function ButtonAppBar() {
 
                     <Button className="LoginButton" color="inherit" sx={{  color: '#c7fc70', mr: 5 }}>Login</Button>
 
-                    <img className="Avatar" src={Avatar} style={{width: '4%'}}/>
+                    <img className="Avatar" src={Avatar} alt='UserAvatar' style={{width: '4%'}}/>
 
 
                 </Toolbar>
