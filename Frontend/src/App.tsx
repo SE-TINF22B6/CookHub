412b50eae64bfbf6cbfc23094fcd3e5458d66969
Stev3eM <stefan-mergl@gmx.de>
2023-12-07 22:00:50 +0100

    // Login-status based redirection // -> set NavBar global -> implemented login-status -> set several pages to be required login-status=true -> removed old header/navbar from { Welcome, Login, AboutUs, FAQsPage, Impressum } -> created PleaseLogin page to catch restricted redirections
    // README //
    -> Login-Status in App.tsx is set 'true' by default until we have an backend logic for the real status (if you want to test the restriction switch to 'false')

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style/Welcome.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, useLocation} from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import NavBar from "./components/NavBar";
 import Welcome from "./pages/Welcome";
@@ -24,10 +24,21 @@ interface RequireAuthProps {
 }
 
 const RequireAuth: React.FC<RequireAuthProps> = ({ children }) => {
-    const loginStatus = false;
+    const loginStatus = true;  // @ backend Team: implement logic for checking if user is logged in
 
     return loginStatus ? <>{children}</> : <Navigate to="/please-login" />;
 }
+
+const AppBarConditional = () => {
+    const location = useLocation();
+    const excludedRoutes = ['/', '/login', '/logout'];
+
+    if (excludedRoutes.includes(location.pathname)) {
+        return null; // Zeigt nichts an, wenn die Route eine der ausgeschlossenen Routen ist
+    }
+
+    return null;      //return <Footer />
+};
 
 
 
@@ -35,6 +46,7 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <AppBarConditional />
                 <NavBar />
                 <Routes>
                     <Route path='/' element={<Welcome/>} />
