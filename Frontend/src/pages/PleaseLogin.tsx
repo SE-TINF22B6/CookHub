6e0dfe7ffac132c40b20cdd5251641ea1a728b2e
Stev3eM <stefan-mergl@gmx.de>
2023-12-07 20:08:36 +0100

    // Login-status based redirection // -> set NavBar global -> implemented login-status -> set several pages to be required login-status=true -> removed old header/navbar from { Welcome, Login, AboutUs, FAQsPage, Impressum } -> created PleaseLogin page to catch restricted redirections

---
+++ Frontend/src/pages/PleaseLogin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PleaseLogin = () => {
+    return (
+        <div>
+            Please login or create an account to access this page.
+        </div>
+    );
+};
+
+export default PleaseLogin;
