02f8054997e740483d71f1c7c6fe07447e1d0452
Stev3eM <stefan-mergl@gmx.de>
2023-12-08 19:07:53 +0100

    // Login-status based redirection // -> set NavBar global -> implemented login-status -> set several pages to be required login-status=true -> removed old header/navbar from { Welcome, Login, AboutUs, FAQsPage, Impressum } -> created PleaseLogin page to catch restricted redirections
    // README //
    -> Login-Status in App.tsx is set 'true' by default until we have an backend logic for the real status (if you want to test the restriction switch to 'false')
    
    // Additions //
    -> Added INFO.responsiveDesign in Helpers directory (contains breakpoints & examples for responsive design)
    -> added links from NavBar to PopupMenu
    -> added icons to PopupMenu
    -> created RecipeDB_Fetcher.tsx and tried to connect to MyRecipes.tsx
    
    // Adjustments //
    -> Welcome page is now responsive
    -> Login page is now responsive
    -> NavBar is now responsive
    -> worked on MyRecipe
    -> complete Overhaul of MyRecipes.tsx (still not full responsive)

--- Frontend/src/helpers/RecipesDB_Simulator.tsx
+++ Frontend/src/helpers/RecipesDB_Simulator.tsx
@@ -1,47 +1,32 @@
-const recipes = "";
-
-export default function RecipesDB_Simulator() {
-    return (
-
-        <div>
-            name = 'Klassische Ramensuppe';
-            zubereitungszeit = '30 min';
-            schwierigkeitsgrad = 'leicht';
-            anzahlPortionen = '4';
-            zutaten = [
-                '1 Zwiebel',
-                '1 Knoblauchzehe',
-                '1 Stück Ingwer',
-                '1 Bund Frühlingszwiebeln',
-                '1 Möhre',
-                '1 Stange Staudensellerie',
-                '1 EL Öl',
-                '1 EL Tomatenmark',
-                '1 EL Sojasauce',
-                '1 EL Fischsauce',
-                '1 TL Sambal Oelek',
-                '1 TL Zucker',
-                '1,5 l Gemüsebrühe',
-                '250 g Ramen-Nudeln',
-                '1 Bund Koriander',
-                '1 Limette',
-                '2 Eier',
-                '2 EL Sesamöl',
-                '2 EL Sesam',
-                'Salz',
-                'Pfeffer'
-            ];
-            anleitung = [
-                'Zwiebel, Knoblauch und Ingwer schälen und fein hacken. Frühlingszwiebeln waschen, putzen und in Ringe schneiden. Möhre und Sellerie schälen und in feine Scheiben schneiden.',
-                'Öl in einem Topf erhitzen und Zwiebel, Knoblauch und Ingwer darin anbraten. Tomatenmark, Sojasauce, Fischsauce, Sambal Oelek und Zucker zugeben und kurz mitbraten. Mit Gemüsebrühe ablöschen und aufkochen lassen.',
-                'Möhre, Sellerie und Frühlingszwiebeln zugeben und ca. 5 Minuten köcheln lassen. Ramen-Nudeln zugeben und nach Packungsanweisung garen.',
-                'Inzwischen Koriander waschen, trocken schütteln und die Blätter abzupfen. Limette waschen, trocken reiben und in Spalten schneiden. Eier in kochendem Wasser ca. 6 Minuten wachsweich kochen. Kalt abschrecken, pellen und halbieren.',
-                'Suppe mit Salz und Pfeffer abschmecken. Suppe in Schalen füllen und mit Koriander, Limettenspalten, Eiern und Sesam bestreut servieren.'
-            ];
-            bild = 'https://img.chefkoch-cdn.de/rezepte/369520159622226/bilder/1042668/crop-600x400/ramen-suppe.jpg';
 
 
-        </div>
-    )
+
+const recipeData = [
+    {
+        name: "Klassische Ramensuppe",
+        rating: "4.5",
+        preparationTime: "30min",
+        cookingTime: "15min",
+        difficulty: "Easy",
+        ingredients: "Liste der Zutaten",
+        instructions: "Kochanleitung"
+    },
+];
+
+
+
+const RecipesDB_Fetcher = () => {
+
+    // Lobic for fetching demanded recipes from database
+    // save them in recipes array
+
+    return 1;
 }
 
+
+export default recipeData;
+
+
+
+let one = RecipesDB_Fetcher();
+console.log(one);
