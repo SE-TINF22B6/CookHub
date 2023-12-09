bb4cf6c261fb82fcd269c04b969927e86f77a4af
Stev3eM <stefan-mergl@gmx.de>
2023-12-09 04:46:31 +0100

    /// BugFix /// -> changed AboutUs page .CSS attributes names due to names collisions to another .css
    /// Responsive ///
    -> AboutUs page is now responsive
    
    /// Content change ///
    -> replaced team_gear picture with Carlos as 5th team member in AboutUs page
    
    /// Development ///
    -> started development of CreateRecipe.tsx & CreateRecipe.css

--- Frontend/src/pages/CreateRecipe.tsx
+++ Frontend/src/pages/CreateRecipe.tsx
@@ -1,36 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import logo from "../assets/Hotdog.svg";
 import "../style/CreateRecipe.css";
 import AddIngredientApp from "../components/AddIngredientApp";
 
 export default function CreateRecipe() {
     return (
-        <div>
-            <Container>
-                <div className={"topContainer"}>
-                    <h1>MY RECIPE</h1>
-                </div>
-            </Container>
-            <Container>
-                <div className={"formContainer"}>
-                    <Row>
-                        <Col>
+
+        <div  id="createRecipeContainer_top" >
+
+            <div id="grid-container" style={{backgroundColor: "#38517d"}}>
+
+                <div className="createRecipeContainer_left"style={{backgroundColor: "#38517d"}}>
+                    <div className={"topContainer"}>
+                        <br/><br/>
+                        <h1>MY NEW RECIPE</h1>
                         <AddIngredientApp></AddIngredientApp>
-                        </Col>
-                        <Col>
-                            <img src={logo} alt={"logo"}/>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <textarea></textarea>
-                        </Col>
-                    </Row>
+                    </div>
                 </div>
-            </Container>
+
+                <div className="createRecipeContainer_right" style={{backgroundColor: "#38517d"}}>
+                    <div className={"formContainer"}>
+                        <textarea className="textArea"></textarea>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
