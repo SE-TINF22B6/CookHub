0c509094ae749becf8473d69bcbbefa783502139
Stev3eM <stefan-mergl@gmx.de>
2024-04-04 19:34:59 +0200

    /// Adjustments /// -> changed name of CreateRecipe.css to FoodForge.css -> created Slider.tsx component and added it to FoodForge.tsx -> replaced pictures in FoodForge.tsx, now there's a pot taking ingredients from user -> small adjustments on NavBar.tsx

--- Frontend/src/pages/FindRecipes.tsx
+++ Frontend/src/pages/FindRecipes.tsx
@@ -1,38 +1,34 @@
 import Grid from '@mui/material/Grid';
 import React from "react";
 import "../style/FindRecipes.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Card from "../components/ComplexCard";
 
-function generateCards() {
+function generateCards(numCards: number) {
     let cards = [];
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < numCards; i++) {
         cards.push(
-            <Grid item md={4} key={i}>
+            <Col xs={12} sm={6} md={4} lg={3} key={i}>
                 <Card/>
-            </Grid>
+            </Col>
         );
     }
     return cards;
 }
 
 
-
 export default function FindRecipes() {
-
-    let cards = generateCards();
-
+    let cards = generateCards(12);
     return (
         <div className="RecipeBrowser-Container">
             <div className="header">
                 <h1>RECIPE BROWSER</h1>
                 <input type="text" placeholder="Search" id="outlined-basic" className="search-input"/>
             </div>
-
-            <div className="cards-container">
+            <Row className="cards-container">
                 {cards}
-                {cards}
-            </div>
+            </Row>
         </div>
-
     );
 }
