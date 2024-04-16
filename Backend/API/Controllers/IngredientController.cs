f84d629f0643a10478b0471619ad98fe2e93d4cb
lvejsada <len.vejsada@kardex.com>
2024-04-16 11:46:58 +0200

    Added Get By ID, Name and Get a list of all Ingredients

--- Backend/API/Controllers/IngredientController.cs
+++ Backend/API/Controllers/IngredientController.cs
@@ -1,7 +1,13 @@
+using DataAccess.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Services;
 
 namespace API.Controllers;
+
+
+/// <summary>
+/// Class that defines api endpoints for ingredients
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class IngredientController: ControllerBase
@@ -11,6 +17,46 @@ public class IngredientController: ControllerBase
     public IngredientController(IngredientService ingredientService)
     {
         _ingredientService = ingredientService;
+    }
+    
+    
+    /// <summary>
+    /// Gets a List of all ingredients
+    /// </summary>
+    [HttpGet]
+    public ActionResult<IEnumerable<Ingredient>> GetAllIngredients()
+    {
+        var ingredients =  _ingredientService.GetAllIngredients();
+        return Ok(ingredients);
+    }
+    
+    
+    /// <summary>
+    /// Gets a ingredient by ID
+    /// </summary>
+    [HttpGet("{id}")]
+    public IActionResult GetIngredient(int? id)
+    {
+        if (id == null)
+        {
+            return BadRequest("ID cannot be null.");
+        }
+
+        var ingredient = _ingredientService.GetIngredientById(id.Value);
+    
+        if (ingredient == null)
+        {
+            return NotFound("Ingredient not found.");
+        }
+
+        return Ok(ingredient);
+    }
+
+    [HttpGet("byname/{name}")]
+    public IActionResult GetIngredientByName(string name)
+    {
+            var ingredients = _ingredientService.GetIngredientByName(name);
+            return Ok(ingredients);
     }
 
 }
--- Backend/Services/IngredientService.cs
+++ Backend/Services/IngredientService.cs
@@ -14,4 +14,22 @@ public class IngredientService
     {
         _repository = repository;
     }
+
+    public IEnumerable<Ingredient> GetAllIngredients()
+    {
+        return _repository.GetAll();
+    }
+    
+    public Ingredient? GetIngredientById(int id)
+    {
+        var ingredient = _repository.Get(id);
+        return ingredient;
+      
+    }
+
+    public List<Ingredient> GetIngredientByName(string name)
+    {
+        var ingredients = _repository.GetAll();
+        return ingredients.Where(r => r.Name.Contains(name)).ToList();
+    }
 }
