a639ad20256cc720c580855f32c6ed9981443698
Nick Hörner <nhoerner@jacob.de>
2024-04-17 13:56:28 +0200

    added search functions for recipes

--- Frontend/src/clients/RecipeClient.ts
+++ Frontend/src/clients/RecipeClient.ts
@@ -1,1 +1,32 @@
-export {}
+import axios from "axios";
+
+export class RecipeClient {
+
+     public async getRecipeByName(name: string) {
+         try {
+         const response = await fetch(`https://localhost:44328/Recipe/byname/${name}`);
+         if (!response.ok) {
+             throw new Error(response.statusText);
+         }
+         const recipes = await response.json();
+         console.log(recipes);
+
+         }catch (error){
+             console.log(error);
+         }
+     }
+
+    public async getAllRecipes() {
+         try {
+        const response = await fetch(`https://localhost:44328/Recipe`);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const recipes = await response.json();
+        console.log(recipes);
+
+         }catch (error){
+             console.log(error);
+         }
+    }
+}
--- Frontend/src/pages/FindRecipes.tsx
+++ Frontend/src/pages/FindRecipes.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import "../style/FindRecipes.css";
+import {RecipeClient} from "../clients/RecipeClient";
+import Button from "@mui/material/Button";
 
 export default function FindRecipes() {
+
+
+    function findBtn():void{
+        let client : RecipeClient = new RecipeClient();
+        client.getRecipeByName("Pizza");
+    }
+
+    function findAll():void {
+        const client = new RecipeClient();
+        client.getAllRecipes();
+    }
+
+
     return (
 
         <div className="RecipeBrowser" >
-
+            <Button onClick={findBtn}>
+                Find Pizza
+            </Button>
+            <Button onClick={findAll}>
+                Find all Recipes
+            </Button>
         </div>
 
     );
