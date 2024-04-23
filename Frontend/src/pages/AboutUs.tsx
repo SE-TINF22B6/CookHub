1e4220754f91b45d5b1b8f55369274aa798e0399
Nick Hörner <nhoerner@jacob.de>
2024-04-23 20:33:41 +0200

    fixed abouts us page

--- Frontend/src/pages/AboutUs.tsx
+++ Frontend/src/pages/AboutUs.tsx
@@ -9,15 +9,11 @@ export default function AboutUs() {
     return (
         <div className="AboutUsPage">
             <div className="bodyAboutUs">
-                <Grid container spacing={2}>
-                    <Grid item xl={6} className="leftSideUs">
-                        <img src={CookHub_Team_H1} className="Team_Roles" alt="team"/>
-                        <img src={Team_listed} className="Team_Roles" alt="team"/>
-                    </Grid>
-                    <Grid item xl={6} className="rightSideUs">
-                        <img src={Carlos} className="CarlosTeamMember" alt="team"/>
-                    </Grid>
-                </Grid>
+                <img src={CookHub_Team_H1} className="Team_Roles" alt="team"/>
+                <img src={Team_listed} className="Team_Roles" alt="team"/>
+            </div>
+            <div className="carlos">
+                <img src={Carlos} className="CarlosTeamMember" alt="team"/>
             </div>
         </div>
     );
