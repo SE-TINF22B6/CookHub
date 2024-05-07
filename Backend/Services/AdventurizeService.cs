f6594d39c1bd72c41d417c94b4d0e6a059c8fa9f
Stev3eM <stefan-mergl@gmx.de>
2024-05-07 21:30:48 +0200

    // AdventurizeService.cs -> Updated prompt to version 1.0

--- Backend/Services/AdventurizeService.cs
+++ Backend/Services/AdventurizeService.cs
@@ -9,23 +9,35 @@ public class AdventurizeService
     private readonly OpenAIService _openAiService;
 
     private const string SystemInstructions =
-        "I need you for a WebApp-Project called CookHub. CookHub is a cooking app providing the searching, saving and" +
-        "creating of recipes. A special function we offer to our customers is the ability to convert any kind of" +
-        "recipe into an adventure story. A story should follow the famous hero journey whereas the protagonist is" +
-        "always the person who's cooking right now. This means you'll write the text in the first-person perspective" +
-        "for the user. Try to get inspiration for the story from famous and high rated stories out of the world of" +
-        "books, movies and series. For each new recipe to create you select a writing style of a world famous writer" +
-        "to preserve the integrity of the story. Stick exactly to the given recipe and the given ingredients in the" +
-        "first place to avoid accidents in taste and the kitchen.\"\n" +
-        "\n" +
-        "A recipe has always the following fromat:\n" +
-        "\n" +
-        "'Name of the recipe'\n" +
-        "'Preperation time'\n" +
-        "'Cooking time'\n" +
-        "'Difficulty'\n" +
-        "'Ingredients'\n" +
-        "'Instructions'";
+        "Convert the given recipe into an adventure story. The story should follow the famous hero journey whereas " +
+        "the protagonist is the person who's cooking this recipe. This means you'll write the text in the " +
+        "first-person perspective for the user. Try to get inspiration for the story from famous and high rated " +
+        "stories out of the world of books, movies and series. For each new recipe to create you select a writing " +
+        "style of a world famous writer to preserve the integrity of the story. Stick exactly to the given recipe " +
+        "and the given ingredients in the first place to avoid accidents in taste and the kitchen. " +
+        "Try not to mention quantity specifications for the ingredients regarding to the ingredients being " +
+        "listed speperately with the posability to get factorized."+
+        
+        "A recipe has always the following format including linebreaks and free lines in between. Don't use markdown language. = " +
+        "{"+
+        "<Creative name of this Recipe>"+
+        "<add free line>"+
+        "<A short intro text to the adventure (max 3-4 sentences)>"+
+        "<add free line>"+
+        "<Chapter name of this adventure step>"+
+        "<add free line>"+
+        "<First step one of the recipe...>"+
+        "<add free line>"+
+        "<Chapter name of next adventure step>"+
+        "<add free line>"+
+        "<Next step of the recipe...>"+
+        "<add free line>"+
+        "...iterate until final step..."+
+        "<add free line>"+
+        "<Final chapter name of the last step>"+
+        "<add free line>"+
+        "<Last step of the adventure with an epic ending of the adventure>" +
+        "}";
 
     public AdventurizeService(OpenAIService openAiService)
     {
