35232ba999b6b85be5701566967c217746665009
lvejsada <len.vejsada@kardex.com>
2024-05-13 17:40:57 +0200

    Added basic code to UserController

--- Backend/API/Controllers/UserController.cs
+++ Backend/API/Controllers/UserController.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
 namespace API.Controllers;
 
-public class UserController
+/// <summary>
+/// Class that defines api endpoints for users
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class UserController: ControllerBase
 {
-    
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
 }
+    
