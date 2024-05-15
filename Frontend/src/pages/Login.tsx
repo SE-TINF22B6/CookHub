48eb9ec19c9255a31593ddf71aed8d587e3d078c
Stev3eM <stefan-mergl@gmx.de>
2024-05-15 16:04:21 +0200

    //LoginPage -> Removed all material ui components and replaced them with divs and labels -> fixed positioning of login form, speech bubble and Carlos

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -3,8 +3,6 @@ import React, {useState} from "react";
 import "../style/Login.css"
 import chef from "../assets/Chef_Carlo_without_background (1).png";
 import {UserClient} from "../clients/UserClient";
-import Grid from '@mui/material/Grid'
-import Item from '@mui/material/Grid'
 
 export default function Login() {
 
@@ -13,59 +11,74 @@ export default function Login() {
     let [message, setMessage] = useState('');
 
     return (
-
         <div id="LoginPage">
-            <Grid id="LoginPageBody" container>
-                <Item>
+            <div id="LoginPageBody">
+                <div className="Item">
                     <div id="LoginPageBodyLeft">
-                        <Grid item md={5}>
-                            <div className="EnterInformation">
-                                <form className={"container col-md-6"}>
-                                    <div className="InputMail">
-                                        <label form="exampleInputEmail1" className="form-label">Email address</label>
-                                        <input type="email" className="form-control" id="exampleInputEmail1"  aria-describedby="emailHelp" onInput={event => email = event.currentTarget.value}></input>
-                                    </div>
-                                    <div className="InputPassword">
-                                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1" onInput={event => password = event.currentTarget.value}></input>
-                                    </div>
-                                    <div style={{fontSize: 12}}>{message}</div>
-                                    <input type="button" className="SubmitLogin" value="Submit" onClick={() => new UserClient().sendLoginRequest(email, password).then(response => setMessage(JSON.parse(response)))}/>
-                                </form>
+                        <div className="EnterInformation">
+                            <div className="InputMail">
+                                <label
+                                    form="exampleInputEmail1"
+                                    className="form-label">Email address</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="exampleInputEmail1"
+                                    aria-describedby="emailHelp"
+                                    onInput={event => email = event.currentTarget.value}></input>
+                            </div>
+                            <div className="InputPassword">
+                                <label
+                                    htmlFor="exampleInputPassword1"
+                                    className="form-label">Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="exampleInputPassword1"
+                                    onInput={event => password = event.currentTarget.value}></input>
+                            </div>
+                            <div style={{fontSize: 12}}>{message}</div>
+                            <input
+                                type="button"
+                                className="SubmitLogin"
+                                value="Submit"
+                                onClick={() =>
+                                    new UserClient().sendLoginRequest(email, password).then(
+                                        response => setMessage(JSON.parse(response)))}/>
+                            <br/>
+                            <h3>
+                                <span style={{fontSize: 18}}>Don't have an account yet?</span>
                                 <br/>
-                                <h3>
-                                    <span style={{fontSize: 18}}>Don't have an account yet?</span>
-                                    <br/>
-                                    <a className="SubmitButton" style={{fontSize: 20}} href='/signup'>Sign up</a>
-                                </h3>
-                            </div>
-
-                        </Grid>
+                                <a className="SubmitButton" style={{fontSize: 20}} href='/signup'>Sign up</a>
+                            </h3>
+                        </div>
                     </div>
-                </Item>
+                </div>
 
-                <Item>
+                <div className="Item">
                     <div id="LoginPageBodyRight">
-                        <Grid item md={7}>
-                            <div className='speech-bubble'>
-                                <span style={{color:'#C9FE71', backgroundColor:'#262525FF', fontWeight:'bold', textShadow: '3'}}>Ready</span> to dive into a world of culinary delights? <br/>
-                                <span style={{color:'#C9FE71', backgroundColor:'#262525FF', fontWeight:'bold'}}>Log in</span> to get access to a treasure trove of recipes,
-                                cooking tips, and foodie inspiration. <br/>
-                                <span style={{color:'#C9FE71', backgroundColor:'#262525FF', fontWeight:'bold'}}>Join</span> our community of cooking enthusiasts
-                                and elevate your kitchen experience. Whether it's your first time here or you're
-                                returning for more culinary adventures, we're thrilled to have you aboard. <br/> <br/>
-                                <span style={{color:'#C9FE71', backgroundColor:'#262525FF', fontWeight:'bold'}}>Let's get coooooking!</span>
-                            </div>
-                            <h2 className="LoginPageText"> </h2>
-                            <div className="Loginpage_images">
-                                <img src={chef} className="Chef-Carlo2" alt="chef" />
-                            </div>
-                        </Grid>
+                        <div className='speech-bubble'>
+                            <span
+                                style={{
+                                    color: '#C9FE71',
+                                    backgroundColor: '#262525FF',
+                                    fontWeight: 'bold',
+                                    textShadow: '3'
+                                }}>
+                                Ready to dive into a world of culinary delights? <br/>
+                                Log in to get access to a treasure trove of recipes, cooking tips, and foodie inspiration. <br/>
+                                Join our community of cooking enthusiasts and elevate your kitchen experience. <br/>
+                                Whether it's your first time here or you're returning for more culinary adventures, we're thrilled to have you aboard.
+                                <br/><br/>
+                                Let's get coooooking!
+                            </span>
+                        </div>
+                        <div className="Chef-Carlos">
+                            <img src={chef} className="Chef-Carlo2" alt="chef"></img>
+                        </div>
                     </div>
-                </Item>
-
-            </Grid>
+                </div>
+            </div>
         </div>
-
     );
 }
