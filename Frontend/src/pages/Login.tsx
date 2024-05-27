506c3b0dbce0954a282a54c52f4b277307ef9a26
Stev3eM <stefan-mergl@gmx.de>
2024-05-27 12:32:26 +0200

    //NavBar -> fixed wrong size change of animated logo
    //Login
    -> Login page is now responsive

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -16,10 +16,8 @@ export default function Login() {
         const result = await new UserClient().userLogin(email, password);
 
         if (result === 200) {
-
             setMessage("Login successful!");
             navigate('/profile');
-
         } else {
             setMessage("Login failed!");
         }
@@ -28,72 +26,78 @@ export default function Login() {
 
     return (
         <div id="LoginPage">
-            <div id="LoginPageBody">
-                <div className="Item">
-                    <div id="LoginPageBodyLeft">
-                        <div className="EnterInformation">
-                            <div className="InputMail">
-                                <label
-                                    form="exampleInputEmail1" onSubmit={e => e.preventDefault()}
-                                    className="form-label">Email address</label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    id="exampleInputEmail1"
-                                    aria-describedby="emailHelp"
-                                    onChange={event => setEmail(event.target.value)}></input>
-                            </div>
-                            <div className="InputPassword">
-                                <label
-                                    htmlFor="exampleInputPassword1"
-                                    className="form-label">Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    id="exampleInputPassword1"
-                                    onChange={event => setPassword(event.target.value)}></input>
-                            </div>
-                            <div style={{fontSize: 12}}>{message}</div>
-                            <input
-                                type="button"
-                                className="SubmitLogin"
-                                value="Submit"
-                                onClick={handleLogin}
-                                   />
-                            <br/>
-                            <h3>
-                                <span style={{fontSize: 18}}>Don't have an account yet?</span>
-                                <br/>
-                                <a className="SubmitButton" style={{fontSize: 20}} href='/signup'>Sign up</a>
-                            </h3>
-                        </div>
+
+
+            <div id="BodyLeft">
+
+                <div id="FormContainer">
+
+                    <div className="InputMail">
+                        <label
+                            form="exampleInputEmail1" onSubmit={e => e.preventDefault()}
+                            className="form-label">Email</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            onChange={event => setEmail(event.target.value)}></input>
                     </div>
+
+                    <div className="InputPassword">
+                        <label
+                            htmlFor="exampleInputPassword1"
+                            className="form-label">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            onChange={event => setPassword(event.target.value)}></input>
+                    </div>
+                    <div style={{fontSize: 12}}>{message}</div>
+                    <input
+                        type="button"
+                        className="SubmitButton"
+                        value="Submit"
+                        onClick={handleLogin}
+                    />
+                    <br/>
+                    <h3 className={"sign-up"}>
+                        <span>Don't have an account yet?</span>
+                    </h3>
+                    <a className="SignUpLink" href='/signup'>Sign up</a>
+
                 </div>
 
-                <div className="Item">
-                    <div id="LoginPageBodyRight">
-                        <div className='speech-bubble'>
-                            <span
-                                style={{
-                                    color: '#C9FE71',
-                                    backgroundColor: '#262525FF',
-                                    fontWeight: 'bold',
-                                    textShadow: '3'
-                                }}>
-                                Ready to dive into a world of culinary delights? <br/>
-                                Log in to get access to a treasure trove of recipes, cooking tips, and foodie inspiration. <br/>
-                                Join our community of cooking enthusiasts and elevate your kitchen experience. <br/>
-                                Whether it's your first time here or you're returning for more culinary adventures, we're thrilled to have you aboard.
-                                <br/><br/>
-                                Let's get coooooking!
-                            </span>
-                        </div>
-                        <div className="Chef-Carlos">
-                            <img src={chef} className="Chef-Carlo2" alt="chef"></img>
-                        </div>
-                    </div>
-                </div>
             </div>
+
+
+            <div id="BodyRight">
+
+                <div className='speech-bubble'>
+                    <span
+                        style={{
+                            color: '#C9FE71',
+                            backgroundColor: '#262525FF',
+                            fontWeight: 'bold',
+                            textShadow: '3'
+                        }}>
+                        Ready to dive into a world of culinary delights? <br/>
+                        Log in to get access to a treasure trove of recipes, cooking tips, and foodie inspiration. <br/>
+                        Join our community of cooking enthusiasts and elevate your kitchen experience. <br/>
+                        Whether it's your first time here or you're returning for more culinary adventures, we're thrilled to have you aboard.
+                        <br/>
+                        <br/>
+                        Let's get coooooking!
+                    </span>
+                </div>
+
+                <div className="Chef-Carlos">
+                    <img src={chef} className="Chef-Carlo2" alt="chef"></img>
+                </div>
+
+            </div>
+
         </div>
     );
 }
