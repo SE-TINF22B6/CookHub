9b4f414a02cd87fc5fde0295f2bc7cf01ae04401
challenger3010 <hoernernick@web.de>
2024-05-28 15:51:31 +0200

    implemented sign up

--- Frontend/src/pages/SignUp.tsx
+++ Frontend/src/pages/SignUp.tsx
@@ -1,11 +1,75 @@
 import '../style/SignUp.css';
 import * as React from 'react';
 import chef from "../assets/Chef_Carlo_without_background (1).png";
+import {useEffect, useState} from "react";
+import {UserClient} from "../clients/UserClient";
+
 
 
 export default function SignUp() {
-    return (
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordRe, setPasswordRe] = useState('');
+    const [userName, setUserName] = useState('');
+    const [message, setMessage] = useState('');
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const usernameRegex = /^[A-Za-z0-9_]{4,16}$/;
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,24}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,32}$/;
+
+    const validateForm = () => {
+        if(userName === ""){
+            setMessage("");
+            return ;
+        }
+        if (!usernameRegex.test(userName)) {
+            setMessage("Username must be 4-16 characters long and can only contain letters, numbers, and underscores.");
+            setIsFormValid(false);
+            return;
+        }
+
+        if (!emailRegex.test(email)) {
+            setMessage("Please enter a valid email address.");
+            setIsFormValid(false);
+            return;
+        }
+
+        if (password !== passwordRe) {
+            setMessage("Passwords don't match!");
+            setIsFormValid(false);
+            return;
+        }
+
+        if (!passwordRegex.test(password)) {
+            setMessage("Password must be 8-32 characters long, contain at least one letter, one number, and one special character.");
+            setIsFormValid(false);
+            return;
+        }
+
+        setMessage("");
+        setIsFormValid(true);
+    };
+
+    useEffect(() => {
+        validateForm();
+    }, [userName,password,email,passwordRe]);
+
+
+
+
+    const handleSignUp = async () => {
+        if (isFormValid) {
+            const response = await new UserClient().userSignup(userName,email,password);
+
+            console.log(response);
+
+            console.log("SignUp called", email, password, passwordRe, userName);
+        }
+    };
+
+    return (
         <div className="SignUpPage">
 
             <div className="headerSignUp">
@@ -21,22 +85,23 @@ export default function SignUp() {
             <div className="bodySignUp">
 
                 <div className="formContainer">
-                    <form>
+                    <form onSubmit={(e) =>{e.preventDefault();}}>
                         <label htmlFor="username">Username:</label><br/>
-                        <input type="text" id="username" name="username"/><br/><br/>
+                        <input type="text" id="username" name="username" onChange={(e)=>setUserName(e.target.value)}/><br/><br/>
 
                         <label htmlFor="email">Email:</label><br/>
-                        <input type="email" id="email" name="email"/><br/><br/><br/>
+                        <input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)}/><br/><br/><br/>
 
                         <label htmlFor="password">Password:</label><br/>
-                        <input type="password" id="password_1" name="password"/><br/><br/>
+                        <input type="password" id="password_1" name="password" onChange={(e) => setPassword(e.target.value)}/><br/><br/>
 
                         <label htmlFor="password">Repeat Password:</label><br/>
-                        <input type="password" id="password_2" name="password"/><br/><br/><br/>
-
-                        <input type="submit" value="Submit"/>
+                        <input type="password" id="password_2" name="password" onChange={(e) => setPasswordRe(e.target.value)}/><br/><br/><br/>
+                        <span style={{width:"200px", color:"red", fontSize: "14px", marginBottom: "30px", display:"block"}}>{message}</span>
+                        <input type="submit" value="Submit" onClick={handleSignUp}/>
                     </form>
                 </div>
+
 
                 <div className="carlosContainer">
 
