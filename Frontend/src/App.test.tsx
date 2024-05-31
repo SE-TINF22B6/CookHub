3697112765ac8207dc63d85d667348bab4c89709
Marc Siegfarth <marc.siegfarth@kardex.com>
2024-05-31 16:39:41 +0200

    install chrome in build script

--- Frontend/src/App.test.tsx
+++ Frontend/src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import {Browser, Builder} from "selenium-webdriver";
+import Chrome from 'selenium-webdriver/chrome';
 
 test('renders learn react link', () => {
     render(<App />);
@@ -10,7 +11,9 @@ test('renders learn react link', () => {
 });
 
 it('should be able to start selenium', async () => {
-    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    const options = new Chrome.Options();
+    options.addArguments('headless');
+    let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
     await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
     let title = await driver.getTitle();
     expect(title).toEqual("Web form");
