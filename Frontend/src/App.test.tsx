acbfa00bc69834f5e4b8789929f18cb2ea2d752e
Marc Siegfarth <marc.siegfarth@kardex.com>
2024-05-31 19:51:20 +0200

    remove broken test and formatting

--- Frontend/src/App.test.tsx
+++ Frontend/src/App.test.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 import {driver} from "./setupTests";
-
-test('renders learn react link', () => {
-    render(<App />);
-    const element = screen.getByText(/Start Journey/i);
-    expect(element).toBeInTheDocument();
-});
 
 it('should be able to start selenium', async () => {
     await driver.get('https://localhost:3000/');
--- Frontend/src/tests/FindRecipes.test.tsx
+++ Frontend/src/tests/FindRecipes.test.tsx
@@ -2,9 +2,9 @@ import {By, until} from "selenium-webdriver";
 import {driver} from "../setupTests";
 
 it('should be able to browse recipes', async () => {
-  await driver.get('https://localhost:3000/findrecipes');
-  driver.wait(until.elementLocated(By.id('recipe1')));
-  let recipe = await driver.findElement(By.id('recipe1'));
-  let recipeName = await recipe.getText();
-  expect(recipeName).toEqual('Pizza Margherita');
+    await driver.get('https://localhost:3000/findrecipes');
+    driver.wait(until.elementLocated(By.id('recipe1')));
+    let recipe = await driver.findElement(By.id('recipe1'));
+    let recipeName = await recipe.getText();
+    expect(recipeName).toEqual('Pizza Margherita');
 })
