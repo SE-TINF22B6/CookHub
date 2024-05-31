14f114a20697a8a91b6b8cb09c2fe866297e3bd4
Marc Siegfarth <marc.siegfarth@kardex.com>
2024-05-31 19:46:57 +0200

    add test for FindRecipes

--- Frontend/src/App.test.tsx
+++ Frontend/src/App.test.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {Browser, Builder} from "selenium-webdriver";
-import Chrome from 'selenium-webdriver/chrome';
-
-jest.setTimeout(60_000);
+import {driver} from "./setupTests";
 
 test('renders learn react link', () => {
     render(<App />);
@@ -13,11 +10,7 @@ test('renders learn react link', () => {
 });
 
 it('should be able to start selenium', async () => {
-    const options = new Chrome.Options();
-    options.addArguments('headless', 'ignore-certificate-errors');
-    let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
     await driver.get('https://localhost:3000/');
     let title = await driver.getTitle();
     expect(title).toEqual("React App");
-    await driver.close();
 });
