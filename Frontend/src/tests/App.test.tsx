18bdd41afff30c1fd0b2d4d2d7e1b5fc45aa9b68
Marc Siegfarth <marc.siegfarth@kardex.com>
2024-06-06 19:47:33 +0200

    add tests for logo redirection

--- Frontend/src/tests/App.test.tsx
+++ Frontend/src/tests/App.test.tsx
@@ -1,7 +1,47 @@
-import {driver, frontendUrl} from "../setupTests";
+import {driver, frontendUrl, logInWithTestUser, logOut} from "../setupTests";
+import {By} from "selenium-webdriver";
 
-it('should be able to start selenium', async () => {
-    await driver.get(frontendUrl);
-    let title = await driver.getTitle();
-    expect(title).toEqual("React App");
+describe('test with logged in user', () => {
+    beforeAll(async () => {
+        await driver.get(frontendUrl);
+        await logInWithTestUser();
+        await driver.navigate().refresh();
+    });
+
+    it('should redirect to the FindRecipes page when clicking logo', async () => {
+        // ARRANGE
+        const logo = await driver.findElement(By.className('Logo'));
+
+        // ACT
+        await logo.click();
+
+        // ASSERT
+        const url = await driver.getCurrentUrl();
+        expect(url).toEqual(`${frontendUrl}/findrecipes`);
+    });
+
+    afterAll(async () => await logOut());
+});
+
+describe('test without logged in user', () => {
+    beforeAll(async () => {
+        await driver.get(frontendUrl);
+    });
+
+    it('should be able to start selenium', async () => {
+        const title = await driver.getTitle();
+        expect(title).toEqual("React App");
+    });
+
+    it('should redirect to the home page when clicking logo', async () => {
+        // ARRANGE
+        const logo = await driver.findElement(By.className('Logo'));
+
+        // ACT
+        await logo.click();
+
+        // ASSERT
+        const url = await driver.getCurrentUrl();
+        expect(url).toEqual(`${frontendUrl}/`);
+    });
 });
