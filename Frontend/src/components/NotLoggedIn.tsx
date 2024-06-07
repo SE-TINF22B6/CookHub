22c9f2f100f5559f626ccc6668783642b8e72b82
Len Vejsada <len.vejsada@gmx.de>
2024-06-07 16:29:01 +0200

    Added style to NotLoggedIn component

--- Frontend/src/components/NotLoggedIn.tsx
+++ Frontend/src/components/NotLoggedIn.tsx
@@ -1,12 +1,23 @@
+import React from 'react';
+import '../style/NotLoggedIn.css';
+import ButtonGroup from "@mui/material/ButtonGroup";
+
 export default function NotLoggedIn() {
-
     return (
-        <>
-            <h1>You are not logged in!</h1>
-            <h1>Please login! or Sign Up</h1>
-            <a href={"/login"}><button>To Login</button></a>
-            <a href={"/signup"}><button>Sign Up</button></a>
-        </>
+        <div className="container">
+            <div className="login-box">
+                <h1 className="login-text">You are not logged in!</h1>
+                <h1 className="login-text">Please login or sign up!</h1>
+                <ButtonGroup
+                    className="button-container">
+                    <a href={"/login"}>
+                        <button className="log-in-button">To Login</button>
+                    </a>
+                    <a href={"/signup"}>
+                        <button className="sign-up-button">Sign Up</button>
+                    </a>
+                </ButtonGroup>
+            </div>
+        </div>
     );
-
 }
